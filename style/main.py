a7eac3e0480d071c713d88e5848d7cf1d79057da
kennethbarrtett <44135632+kennethbarrtett@users.noreply.github.com>
2018-12-26 17:06:32 -0500

    Update main.py

--- style/main.py
+++ style/main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect("http://affsuite.xyz/click.php?key=ckq8rrtqeop4vhyrw5sm", True)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
